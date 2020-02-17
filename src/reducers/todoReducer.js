91d082e4579e98d5746541afa47fb03e3cfa6b39
cedric-winbush <cawinbushjr@gmail.com>
2020-02-17 13:29:31 -0600

    add initialstate and todoreducer

---
+++ src/reducers/todoReducer.js
@@ -0,0 +1,15 @@
+import uuid from 'react-uuid';
+
+
+export const initialState = {
+  todos = []
+  item: 'Learn about reducers',
+  completed: false, 
+  id: uuid()
+}
+
+export const todoReducer = (state, action) => {
+
+
+  return state;
+}
